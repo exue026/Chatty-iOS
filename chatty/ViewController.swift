e077b38e7fca7e9931ce2ea2c474ea136239a2ed
Ethan Xue <ethan.xue@gmail.com>
2017-07-08 22:18:25 -0400

    started project

---
+++ chatty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chatty
+//
+//  Created by Ethan Xue on 2017-07-08.
+//  Copyright © 2017 xTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
